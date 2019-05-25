f1a899909f61b6244a008be9ba23207f4b2441a4
wyj16340227 <18099228865@163.com>
2019-05-25 12:00:08 +0800

    publisher-subscribe mode

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Observer {
+    private PlayerState playerState;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public override void Reaction(PlayerState _playerState)
+    {
+        playerState = _playerState;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
